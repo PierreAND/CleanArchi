dee1868720ef2ab59589e0ebe865644c541124f7
Pierreand <andrieu.ps@gmail.com>
2024-04-06 11:59:02 +0200

    model post

---
+++ src/domains/model/post.ts
@@ -0,0 +1,10 @@
+export class PostModel {
+    id: number,
+    content: string,
+    CreatedAt: Date,
+    UpdateAt: Date
+
+    constructor(post: Partial<PostModel>){
+        Object.assign(this, post);
+    }
+}
